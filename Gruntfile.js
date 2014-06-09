c72e0f9eeb5682ef99963622d9743d0ad6597854
Bjarki Sorens <bjerkins8@gmail.com>
2014-06-09 09:07:06 +0000

    fixing dependencies and updating readme

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,10 @@
-// Generated on 2014-05-28 using generator-angular 0.8.0
-'use strict';
 
-// # Globbing
-// for performance reasons we're only matching one level down:
-// 'test/spec/{,*/}*.js'
-// use this if you want to recursively match all subfolders:
-// 'test/spec/**/*.js'
+'use strict';
 
 module.exports = function (grunt) {
 
   // Load grunt tasks automatically
   require('load-grunt-tasks')(grunt);
-
-  // Time how long tasks take. Can help when optimizing build times
-  require('time-grunt')(grunt);
 
   // Define the configuration for all the tasks
   grunt.initConfig({
--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 angular-datafetcher
 ===========
 
-Generic datafetcher that returns promises.
+Generic datafetcher for GET, POST, PUT, DELETE and returns promises.
+
+#Requirements
+
+- [AngularJS](http://angularjs.org/)
+
+## Testing
+
+`$ grunt test`
+
+This will make Grunt run the linting task and then execute the unit tests.
+
+## Usage
